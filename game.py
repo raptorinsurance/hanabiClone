f1aea842a6d84b23fa96423900cddbfa8d7dc145
Alex Jeffreys <alexjeffreys@Alexs-MacBook-Pro.local>
2016-05-29 18:32:37 +0100

    make game call stub for prompting player actions

--- game.py
+++ game.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
-from player import Player
+import itertools
+import command_line_game_displayer as displayer
 
-class Game():
+class Game(object):
 
     def __init__(self, displayer):
-        displayer.prompt_for_players()
+        self.displayer = displayer
+        self.players = displayer.prompt_for_players()
+        self.start_game()
 
-    def get_players(self):
-        return self.players
+    def start_game(self):
+        # TODO: max_calls currently only there to prevent inifinite loop,
+        # replace with win condition tester
+        max_calls = 10
+        for player in itertools.islice(itertools.cycle(self.players), 0, max_calls):
+            self.displayer.prompt_for_action(player)
 
 
 if __name__ == "__main__":
-    game()
+    displayer = displayer.Displayer()
+    game = Game(displayer)
--- test_game.py
+++ test_game.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
+"""Test module for the Game class"""
 
 import unittest
 import unittest.mock as mock
 
 from game import Game
 
+
 class TestGame(unittest.TestCase):
+    """Test class for Game"""
 
     def test_start_new_game_prompts_for_number_of_players(self):
-        gameDisplayer = mock.Mock()
+        gameDisplayer = mock.MagicMock()
         game = Game(gameDisplayer)
         gameDisplayer.prompt_for_players.assert_any_call()
+
+    def test_start_new_game_prompts_players_in_turn_for_action(self):
+        gameDisplayer = mock.MagicMock()
+        gameDisplayer.prompt_for_players.return_value =  ["alex", "bob"]
+        game = Game(gameDisplayer)
+
+        expected_first_calls = [mock.call("alex"), mock.call("bob"), mock.call("alex")]
+        self.assertEqual(gameDisplayer.prompt_for_action.call_args_list[:3],
+                          expected_first_calls)
+
 
 
 if __name__ == "__main__":
