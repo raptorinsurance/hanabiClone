07e9e2463de63a6de9da5bd8d0e4528fa53e9ccd
Alex Jeffreys <alexjeffreys@Alexs-MacBook-Pro.local>
2016-06-03 22:45:44 +0100

    make command line adding of player names go one by one

--- command_line_game_displayer.py
+++ command_line_game_displayer.py
@@ -1,11 +1,27 @@
+ORDINALS = ["first", "second", "third"]
+MIN_PLAYERS = 2
+
+def get_input(text):
+    """Wrapper for input to allow patching for unit tests"""
+    return input(text)
+
 class CommandLineGameDisplayer(object):
 
     def __init__(self):
-        pass
-
-    def get_input(text):
-        """Wrapper for input to allow patching for unit tests"""
-        return input(text)
+        self.players = []
 
     def prompt_for_players(self):
-        pass
+        for i in range(len(ORDINALS)):
+            if len(self.players) == MIN_PLAYERS:
+                self._another_player_prompt()
+            self._prompt_for_nth_player()
+        return self.players
+
+    def _another_player_prompt(self):
+        another_player_prompt = "Would you like to add a third player?"
+        get_input(another_player_prompt)
+
+    def _prompt_for_nth_player(self):
+        name_prompt = ("Please enter the name of the {ordinal} player: "
+            .format(ordinal=ORDINALS[len(self.players)]))
+        self.players.append(get_input(name_prompt))
--- test_command_line_game_displayer.py
+++ test_command_line_game_displayer.py
@@ -4,13 +4,33 @@ import unittest.mock as mock
 from command_line_game_displayer import CommandLineGameDisplayer
 
 
+@mock.patch('command_line_game_displayer.get_input')
 class TestCommandLineGameDisplayer(unittest.TestCase):
 
-    @mock.patch('CommandLineGameDisplayer.get_input', return_value="alex")
-    def test_players_prompt(self):
-        game_displayer = CommandLineGameDisplayer()
-        players = game_displayer.prompt_for_players()
-        self.assertEqual(players, ["alex", "bob"])
+    def setUp(self):
+        self.game_displayer = CommandLineGameDisplayer()
+
+    def test_player_name_prompt_first_two_players(self, mock_input):
+        self.game_displayer.prompt_for_players()
+        expected_calls = []
+        expected_calls.append(mock.call("Please enter the name of the first player: "))
+        expected_calls.append(mock.call("Please enter the name of the second player: "))
+        mock_input.assert_has_calls(expected_calls)
+
+    def test_ask_for_third_player(self, mock_input):
+        self.game_displayer.prompt_for_players()
+        expected_calls = []
+        expected_calls.append(mock.call("Would you like to add a third player?"))
+        mock_input.assert_has_calls(expected_calls)
+
+    def test_return_players_when_no_extra_players_requested(self, mock_input):
+        expected_players = ["Alex", "Bob"]
+        mock_input.side_effect = expected_players.append("n")
+        actual_players = self.game_displayer.prompt_for_players()
+        self.assertEqual(expected_players, actual_players)
+
+
+
 
 
 
